e4a3685805d9450c359d6d33f1e5380a220f4b0b
Jaodem <orellana.joseaugusto@gmail.com>
2023-01-03 17:44:30 -0300

    ejercicios resuesltos

---
+++ ejercicios.sql
@@ -0,0 +1,155 @@
+-- Ejercicio 1.
+SELECT nombre FROM Usuarios;
+
+-- Ejercicios 2.
+SELECT MAX(saldo) FROM Usuarios WHERE sexo = 'M';
+
+-- Ejercicio 3.
+SELECT nombre, telefono FROM Usuarios WHERE marca IN('NOKIA', 'BLACKBERRY', 'SONY');
+
+-- Ejercicio 4.
+SELECT COUNT(*) FROM Usuarios WHERE NOT activo OR saldo <= 0;
+
+-- Ejercicio 5.
+SELECT usuario FROM Usuarios WHERE nivel IN(1, 2, 3);
+
+-- Ejercicio 6.
+SELECT telefono FROM Usuarios WHERE saldo <= 300;
+
+-- Ejercicio 7.
+SELECT SUM(saldo) FROM Usuarios WHERE compania = 'NEXTEL';
+
+-- Ejercicio 8.
+SELECT compania, COUNT(*) FROM Usuarios GROUP BY compania;
+
+-- Ejercicio 9.
+SELECT nivel, COUNT(*) FROM Usuarios GROUP BY nivel;
+
+-- Ejercicio 10.
+SELECT usuario FROM Usuarios WHERE nivel = 2;
+
+-- Ejercicio 11.
+SELECT email FROM Usuarios WHERE email LIKE '%@gmail.com';
+
+-- Ejercicio 12.
+SELECT nombre, telefono FROM Usuarios WHERE marca IN('LG', 'SAMSUNG', 'MOTOROLA');
+
+-- Ejercicio 13.
+SELECT nombre, telefono FROM Usuarios WHERE marca NOT IN('LG', 'SAMSUNG');
+
+-- Ejercicio 14.
+SELECT usuario, telefono FROM Usuarios WHERE compania = 'IUSACELL';
+
+-- Ejercicio 15.
+SELECT usuario, telefono FROM Usuarios WHERE compania <> 'IUSACELL';
+
+-- Ejercicio 16.
+SELECT AVG(saldo) FROM Usuarios WHERE marca = 'NOKIA';
+
+-- Ejercicio 17.
+SELECT usuario, telefono FROM Usuarios WHERE compania IN('IUSACELL', 'AXEL');
+
+-- Ejercicio 18.
+SELECT email FROM Usuarios WHERE email NOT LIKE '%@yahoo.com';
+
+-- Ejercicio 19.
+SELECT usuario, telefono FROM Usuarios WHERE compania NOT IN('TELCEL', 'IUSACELL');
+
+-- Ejercicio 20.
+SELECT usuario, telefono FROM Usuarios WHERE compania = 'UNEFON';
+
+-- Ejercicio 21.
+SELECT DISTINCT marca FROM Usuarios ORDER BY marca DESC;
+
+-- Ejercicio 22.
+SELECT DISTINCT compania FROM Usuarios ORDER BY RAND();
+
+-- Ejercicio 23.
+SELECT usuario FROM Usuarios WHERE nivel IN(0, 2);
+
+-- Ejercicio 24.
+SELECT AVG(saldo) FROM Usuarios WHERE marca = 'LG';
+
+-- Ejercicio 25.
+SELECT usuario FROM Usuarios WHERE nivel IN(1, 3);
+
+-- Ejercicio 26.
+SELECT nombre, telefono FROM Usuarios WHERE marca <> 'BLACKBERRY';
+
+-- Ejercicio 27.
+SELECT usuario FROM Usuarios WHERE nivel = 3;
+
+-- Ejercicio 28.
+SELECT usuario FROM Usuarios WHERE nivel = 0;
+
+-- Ejercicio 29.
+SELECT usuario FROM Usuarios WHERE nivel = 1;
+
+-- Ejercicio 30.
+SELECT sexo, COUNT(*) FROM Usuarios GROUP BY sexo;
+
+-- Ejercicio 31.
+SELECT usuario, telefono FROM Usuarios WHERE compania = 'AT&T';
+
+-- Ejercicio 32.
+SELECT DISTINCT compania FROM Usuarios ORDER BY compania DESC;
+
+-- Ejercicio 33.
+SELECT usuario FROM Usuarios WHERE NOT activo;
+
+-- Ejercicio 34.
+SELECT telefono FROM Usuarios WHERE saldo = 0;
+
+-- Ejercicio 35.
+SELECT MIN(saldo) FROM Usuarios WHERE sexo = 'M';
+
+-- Ejercicio 36.
+SELECT telefono FROM Usuarios WHERE saldo > 300;
+
+-- Ejercicio 37.
+SELECT marca, COUNT(*) FROM Usuarios GROUP BY marca;
+
+-- Ejercicio 38.
+SELECT nombre, telefono FROM Usuarios WHERE marca <> 'LG';
+
+-- Ejercicio 39.
+SELECT DISTINCT compania FROM Usuarios ORDER BY compania ASC;
+
+-- Ejercicio 40.
+SELECT SUM(saldo) FROM Usuarios WHERE compania = 'UNEFON';
+
+-- Ejericio 41.
+SELECT email FROM Usuarios WHERE email LIKE '%@hotmail.com';
+
+-- Ejercicio 42.
+SELECT nombre FROM Usuarios WHERE NOT activo OR saldo = 0;
+
+-- Ejercicio 43.
+SELECT usuario, telefono FROM Usuarios WHERE compania IN('IUSACELL', 'TELCEL');
+
+-- Ejercicio 44.
+SELECT DISTINCT marca FROM Usuarios ORDER BY marca ASC;
+
+-- Ejercicio 45.
+SELECT DISTINCT marca FROM Usuarios ORDER BY RAND();
+
+-- Ejercicio 46.
+SELECT usuario, telefono FROM Usuarios WHERE compania IN('IUSACELL', 'UNEFON');
+
+-- Ejercicio 47.
+SELECT nombre, telefono FROM Usuarios WHERE marca NOT IN('MOTOROLA', 'NOKIA');
+
+-- Ejercicio 48.
+SELECT SUM(saldo) FROM Usuarios WHERE compania = 'TELCEL';
+
+-- Ejercicio 49
+SELECT provincias.nombre FROM provincias
+  LEFT JOIN clientes
+  ON clientes.codigoProvincia=provincias.codigo
+  WHERE clientes.codigoprovincia IS null;
+
+-- Ejercicio 50
+SELECT DISTINCT provincias.nombre FROM provincias
+  LEFT JOIN clientes
+  ON clientes.ciudad=provincias.nombre
+  WHERE clientes.ciudad IS NOT null;
